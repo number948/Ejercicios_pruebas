23a368cf52c78cf1ba3d76c2884c569a3e39bef5
Ashley Caroca <scottsykotom@gmail.com>
2023-06-29 13:12:05 -0400

    ejercicios que busca las ocurrencias de un numero en un array

---
+++ number_occurrences.py
@@ -0,0 +1,24 @@
+def occurrences():
+  myArray = [1,2,2,5,4,6,7,8,8,8]
+  posicion = myArray[0]
+  number = 0
+  longest = 0
+
+  for i in range(1, len(myArray)):
+    if myArray[i] == posicion:
+        posicion = myArray[i]
+        number = posicion
+        longest += 1
+    
+    else: #si no se cumple de que myArray[i] == posicion:
+      if longest >= 2:
+          number += 1
+          posicion = myArray[i]
+  
+  if longest >= 2:
+     number += 1
+        
+  print("number:",number, "ocurrencias:", longest)
+
+if __name__ == '__main__':
+    occurrences()
